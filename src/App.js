e01d98f437da8f3935475e6cffb74953152e5071
giorgi2 <sanikidzegio50@gmail.com>
2024-03-12 20:54:57 +0400

    change standart css to Tailwind css

--- src/App.js
+++ src/App.js
@@ -4,100 +4,94 @@ import words from './WordList.json'
 
 function App() {
   const wordToGuess = useState(words[Math.floor(Math.random() * words.length)])
-  const word = wordToGuess[0].split('')
+  const splitedWord = wordToGuess[0].split('')
   const [guessedLetters, setGuessedLetters] = useState([])
-  console.log(word);
 
   const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
   const incorectletters = guessedLetters.filter(
-    letter => !word.includes(letter)
+    letter => !splitedWord.includes(letter)
   )
   const isLooser = incorectletters.length === 6
-  const isWinner = word.every(letter => 
+  const isWinner = splitedWord.every(letter => 
     guessedLetters.includes(letter)
   )
 
 
-
-  
-
-
-
   const head = (
-    <div style={{ width: '50px', height: '50px', border: '10px solid black', borderRadius: '100%', position: 'absolute', top: '50px', right: '-20px'}}></div> 
+    <div className='border-10 border-solid border-black rounded-full w-50 h-50 absolute top-50px -right-5'></div> 
   )
 
   const body = (
-    <div style={{ width: '10px', height: '100px', backgroundColor: 'black', position: 'absolute', top: '100px', right: '0' }}></div>
+    <div className='w-2.5 h-100 absolute bg-black top-100px right-0'></div>
   )
 
   const leftArm = (
-    <div style={{ width: '100px', height: '10px', backgroundColor: 'black', position: 'absolute', top: '130px', right: '10px', rotate: '30deg', transformOrigin: 'right bottom'}}></div>
+    <div className='w-100px h-2.5 bg-black absolute top-130px right-2.5 rotate-30 origin-bottom-right'></div>
   )
 
   const rightArm = (
-    <div style={{ width: '100px', height: '10px', backgroundColor: 'black', position: 'absolute', top: '130px', right: '-100px', rotate: '-30deg', transformOrigin: 'left bottom'}}></div>
+    <div className='w-100px h-2.5 bg-black absolute top-130px -right-100px -rotate-30 origin-bottom-left' ></div>
   )
 
   const leftLeg = (
-    <div style={{ width: '100px', height: '10px', backgroundColor: 'black', position: 'absolute', top: '190px', right: '0px', rotate: '-50deg', transformOrigin: 'right bottom'}}></div>
+    <div className='w-100px h-2.5 bg-black absolute top-190px right-0 -rotate-50 origin-bottom-right' ></div>
   )
 
   const rightLeg = (
-    <div style={{ width: '100px', height: '10px', backgroundColor: 'black', position: 'absolute', top: '190px', right: '-90px', rotate: '50deg', transformOrigin: 'left bottom'}}></div>
+    <div className='w-100px h-2.5 bg-black absolute top-190px -right-90px rotate-50 origin-bottom-left' ></div>
   )
 
   
 
   return (
     <>
-      <div style={{display: isLooser || isWinner ? 'flex' : 'none', borderRadius: '20px', gap: '30px', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',  position: 'absolute', width: '500px', height: '500px', backgroundColor: 'green', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '3', padding: '20px' }}>
-        <h1 style={{fontSize: '50px'}}>
+      <div className={`${isLooser || isWinner ? 'flex' : 'hidden'} rounded-20 gap-30 flex-col justify-center items-center absolute bg-green-600 w-500px h-500px top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 z-10 p-5`} >
+        <h1 className='size-5xl'>
           {isLooser && 'you Lost'}
           {isWinner && 'you win'}
         </h1>
-        <h1 style={{fontSize: '50px'}}>{isLooser && `word was "${wordToGuess[0]}"` }</h1>
-        <button style={{padding: '20px 60px',border: 'none', borderRadius: '20px', backgroundColor: 'blue', color: 'white'}} onClick={() => {
+        <h1 className='size-5xl'>{isLooser && `word was "${wordToGuess[0]}"` }</h1>
+        <button className='px-60 py-5 rounded-20 border-none bg-blue-700 text-white' onClick={() => {
           window.location.reload()
         }}>Restart</button>
       </div>
-      <div className="flex flex-col items-center justify-center h-full mt-10" style={{opacity: isLooser || isWinner ?  '0.3' : '1'}}>
-        <div style={{ position: 'relative' }}>
+      <div className={`${isLooser || isWinner ? 'opacity-30' : 'opacity-100'} flex flex-col items-center justify-center h-full mt-10`} >
+        <div className='relative'>
           {incorectletters.length >=1 ? head : null}
           {incorectletters.length >= 2 ? body: null}
           {incorectletters.length >= 3 ? leftArm : null}
           {incorectletters.length >= 4 ? rightArm : null}
           {incorectletters.length >= 5 ? leftLeg : null}
           {incorectletters.length >= 6 ? rightLeg : null}
-          <div style={{ position: 'absolute', right: 0, top: 0, height: '50px', width: '10px', backgroundColor: 'black' }}></div>
-          <div style={{width: '200px', height: '10px', backgroundColor: 'black', marginLeft: '120px'}}></div>
-          <div style={{width: '10px', height: '300px', backgroundColor: 'black', marginLeft: '120px'}}></div>
-          <div style={{height: '10px', width: '250px', backgroundColor: 'black'}}></div>
+          <div className='absolute right-0 top-0 h-50px w-2.5 bg-black' ></div>
+          <div className='w-200px h-2.5 bg-black ml-120px' ></div>
+          <div className='w-2.5 h-300px bg-black ml-120px' ></div>
+          <div className='h-2.5 w-250px bg-black'  style={{height: '10px', width: '250px', backgroundColor: 'black'}}></div>
         </div>
 
-        <div className='sm:text-2xl lg:text-8xl gap-2 sm:gap-4 md:gap-8 lg:gap-16'  style={{ display: 'flex', alignItems: 'center',  fontSize: '6rem',  fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'monospace', marginTop: '30px'}}>
-          {word.map((letter) => (
-            <span style={{fontSize: '30px', borderBottom: '.15em solid black'}}>
+        <div className='sm:text-2xl lg:text-8xl gap-2 sm:gap-4 md:gap-8 lg:gap-16 flex items-center text-8xl font-bold uppercase font-mono mt-30px '  >
+          {splitedWord.map((letter) => (
+            <span className='text-3xl border-b-4 border-solid border-black' >
               <span style={{visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden'}}>{letter}</span>
             </span>
           ))}
         </div>
 
 
-        <div className='sm:mx-7 mx-5 md:mx-20 lg:mx-60' style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))', gap: '.5rem', alignSelf: 'stretch', marginTop: '50px'}}>
+        <div className='sm:mx-7 mx-5 md:mx-20 lg:mx-60 grid gap-2 self-stretch mt-50px grid-cols-full' >
           {letters.map((key) => (
             <button onClick={(e) => {
-              if (word.includes(key)) {
-                e.target.className = 'active'
+              if (splitedWord.includes(key)) {
+                e.target.className = 'active p-1.5 uppercase border-4 border-solid border-black text-black font-bold cursor-pointer text-3xl'
               } else {
-                e.target.className = 'inactive'
+                e.target.className = 'inactive p-1.5 uppercase border-4 border-solid border-black text-black font-bold cursor-pointer text-3xl'
               }
               if (!guessedLetters.includes(key)) {
                 setGuessedLetters(currentLetters => [...currentLetters, key])
               }
-            }} disabled={isWinner || isLooser} key={key} style={{padding: '5px',  textTransform: 'uppercase', border: '3px solid black', color: 'black', fontWeight: 'bold', cursor: 'pointer', fontSize: '30px'}}>{key }</button>
+            }} disabled={isWinner || isLooser} key={key} className='p-1.5 uppercase border-4 border-solid border-black text-black font-bold cursor-pointer text-3xl' >{key }</button>
             ))}
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,47 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '10' : '10px'
+      },
+      width: {
+        '50': '50px',
+      },
+      height: {
+        '50': '50px',
+        '100': '100px'
+      },
+      rotate: {
+        '30': '30deg',
+        '50' : '50deg'
+      },
+      spacing: {
+        '30px' : '30px',
+        '50px': '50px',
+        '90px' : '90px',
+        '100px': '100px',
+        '120px': '120px',
+        '130px': '130px',
+        '190px': '190px',
+        '200px': '200px',
+        '250px' : '250px',
+        '300px' : '300px',
+        '500px' : '500px'
+      },
+      borderRadius: {
+        '20' : '20px'
+      },
+      gap: {
+        '30' : '30px'
+      },
+      padding: {
+        '60': '60px'
+      },
+      gridTemplateColumns: {
+        'full' : 'repeat(auto-fit, minmax(75px, 1fr))'
+      }
+    }
   },
   plugins: [],
 }
